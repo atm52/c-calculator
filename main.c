b2ccb0b6c8790cc35d61dc6a52f834d54be05a1e
Alex20games <alexbroersma20@gmail.com>
2025-12-14 23:46:35 +0100

    added begin
    started simpel

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 
 int main()
 {
-  
+    int firtNum, secondNum, result;
+    printf("Hi welcome to my C calculator!\n");
+    printf("Please enter the first number: ");
+    scanf("%d", &firtNum);
+    printf("Please enter the second number: ");
+    scanf("%d", &secondNum);
+    result = firtNum + secondNum;
+    printf("The result is: %d\n", result);
+    return 0;
 }
